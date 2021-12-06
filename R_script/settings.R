7c12fcfdec7ec7bed77bfbbe411df2c9a865cf35
JangYiUk <janglw123@gmail.com>
2021-12-06 22:49:53 +0900

    install h2o packages

--- R_script/settings.R
+++ R_script/settings.R
@@ -4,5 +4,27 @@ path_workspace = "C:/bigdata/github/why-h2o/R_script"
 setwd(path_workspace)
 getwd()
 
-# 
+
+# install package h2o
+install.packages("h2o")
+
+
+# load library
+library(h2o)
+
+
+# init h2o
+h2o.init(ip = "127.0.0.1", port = 54321, startH2O = T)
+
+
+# h2o flow
+# http://localhost:54321/flow/index.html
+
+
+# get h2o version
+h2o.getVersion() # "3.34.0.3"
+
+
+# shut down h2o
+h2o.shutdown()
 
